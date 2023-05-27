388556cd2c42926d93bbdc81c6fe59eedad8804b
LeonardoGarciah <leonardoformulaum@hotmail.com>
2023-05-27 16:00:54 -0300

    feat: signIn with github

---
+++ app/memories.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+const Memories = () => {
+  return (
+    <View className='flex-1 items-center justify-center'><Text>Memories</Text></View>
+  )
+}
+
+export default Memories;
